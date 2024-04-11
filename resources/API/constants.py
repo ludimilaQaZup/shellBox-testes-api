703442d4d61ec782d52c75b5c8c0748297015e4f
Diógenes Chagas <diogenes.chagas@zup.com.br>
2024-04-10 23:28:29 -0300

    Delete resources/API/constants.py

--- resources/API/constants.py
+++
@@ -1,5 +0,0 @@
-# PATHS ShellBox
-
-bff_host = 'https://apihml.digitalportoseguro.com.br/'
-solicitar_abastecimento = 'cliente/v1/abastecimento/shellbox/validar/abastecimento'
-autorizar_abastecimento = 'cliente/v1/abastecimento/shellbox/autorizar/abastecimento'
